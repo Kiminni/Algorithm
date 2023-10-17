60355d673efe292cb1ecec744f11a60ab3fc32f5
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-10-17 22:18:48 +0900

    [level unrated] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,8 @@
+def solution(binomial):
+    tmp = binomial.split(" ")
+    if tmp[1] == "+":
+        return int(tmp[0]) + int(tmp[2])
+    if tmp[1] == "-":
+        return int(tmp[0]) - int(tmp[2])
+    if tmp[1] == "*":
+        return int(tmp[0]) * int(tmp[2])
