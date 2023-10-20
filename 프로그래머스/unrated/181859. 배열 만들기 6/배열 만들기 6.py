155ce0340edd2946afe25863085e5086fa6ad96a
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-10-20 23:42:07 +0900

    [level unrated] Title: 배열 만들기 6, Time: 17.27 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -1,15 +1,21 @@
 def solution(arr):
     stk = []
     for i in range(len(arr)):
-        if stk == []:
-            stk.append(arr[i])
         
-        elif stk != [] and stk[-1] == arr[i]:
+        if stk != [] and stk[-1] == arr[i]:
             stk.pop(-1)
-        
-        elif stk != [] and stk[-1] != arr[i]:
+        else:
             stk.append(arr[i])
     
     if stk == []:
         return [-1]
     return stk
+
+#     stk = []
+#     for i in range(len(arr)):
+#         if stk and stk[-1] == arr[i]:
+#             stk.pop()
+#         else:
+#             stk.append(arr[i])
+
+#     return stk or [-1]
