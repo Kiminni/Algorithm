57d32b69f791e7b29bd7d936aec0b71771748173
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-10-20 23:47:13 +0900

    [level unrated] Title: 무작위로 K개의 수 뽑기, Time: 1979.72 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -0,0 +1,12 @@
+def solution(arr, k):
+    answer = []
+    for i in arr:
+        if i not in answer:
+            answer.append(i)
+    
+    if len(answer) > k:
+        answer = answer[:k]
+    
+    else: 
+        answer.extend([-1] * (k - len(answer)))
+    return answer
