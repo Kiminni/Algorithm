9cdbab1bb95701018e1faf13e8d05d2897424159
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-10-22 20:18:04 +0900

    [level unrated] Title: 문자열 묶기, Time: 11.17 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
@@ -3,7 +3,6 @@ def solution(strArr):
     count_list = [0]*31
     
     for i in range(len(strArr)):
-        strArr[i] = "a" * len(strArr[i])
         count_list[len(strArr[i])] += 1
   
     return max(count_list)
