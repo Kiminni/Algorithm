d42ee709d5e988de09ebedb4c2ed254e3355a7bb
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-11-04 01:54:13 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,8 +1,5 @@
 def solution(num_list):
     answer = [0,0]
-    for i in range(len(num_list)):
-        if num_list[i] % 2 == 0:
-            answer[0] += 1
-        else:
-            answer[1] += 1
+    for i in num_list:
+        answer[i%2] += 1
     return answer
