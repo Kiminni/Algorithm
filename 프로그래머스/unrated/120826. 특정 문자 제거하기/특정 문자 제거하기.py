2069eb21519be6c2b4d427d04dda7f034481c32d
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-11-05 01:57:56 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,7 +1,2 @@
 def solution(my_string, letter):
-    tmp = ''
-    for i in range(len(my_string)):
-        if my_string[i] == letter:
-            continue
-        tmp += my_string[i]
-    return tmp
+    return my_string.replace(letter, '')
