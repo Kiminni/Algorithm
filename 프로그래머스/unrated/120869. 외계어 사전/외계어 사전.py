72619b71fb8e7d94ca72c30e3fcbd65db307992d
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-11-19 17:40:41 +0900

    [level unrated] Title: 외계어 사전, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.py
@@ -1,11 +1,9 @@
 def solution(spell, dic):
     
-    for dict in dic:
-        cnt = 0
-        for s in spell:
-            if s in dict:
-                cnt += 1
-        if len(spell) == cnt:
+    for d in dic:
+        if not set(spell) - set(d):
             return 1
-        
     return 2
+
+
+
