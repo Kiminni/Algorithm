fcab73121d59b93eb7f1758191b218a54ae67848
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-12-14 23:54:52 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,9 +1,17 @@
-import math
+# import math
 
-def solution(n):
-    result = math.sqrt(n)
-    if result % 1 != 0 :
-        return -1 
-    return (result+1) ** 2
+# def solution(n):
+#     result = math.sqrt(n)
+#     if result % 1 != 0 :
+#         return -1 
+#     return (result+1) ** 2
+
+def solution(n) :
+    sqrt = n ** (1/2)
+
+    if sqrt % 1 == 0:
+        return (sqrt + 1) ** 2
+    
+    return -1
 
     
