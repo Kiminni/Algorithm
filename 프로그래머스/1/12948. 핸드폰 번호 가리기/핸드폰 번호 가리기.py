0bf89d52734ee07093ad5ea696938985433f07f7
Kiminni <106671921+Kiminni@users.noreply.github.com>
2023-12-31 23:51:34 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,7 +1,3 @@
 def solution(phone_number):
-    length = len(phone_number)
-    answer = ''
-    for i in phone_number:
-        if length - len(answer) > 4:
-            answer += "*"
-    return answer + phone_number[len(phone_number) - 4:]
+
+    return "*"*(len(phone_number) - 4) + phone_number[-4:]
