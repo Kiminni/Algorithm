a8a6a78fbf14676d97352ed1ee73d9fa4e5746d5
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-03-05 02:36:49 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,8 +1,3 @@
 def solution(n):
-    answer = ""
-    for i in range(n):
-        if i % 2 == 0:
-            answer += "수"
-        else:
-            answer += "박"
-    return answer
+    str = "수박" * n
+    return str[:n]
