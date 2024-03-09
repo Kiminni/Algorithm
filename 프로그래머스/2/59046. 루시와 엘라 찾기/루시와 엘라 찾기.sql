bfd50919e0b726384d69d4faa1ff0e4c018d3f2f
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-03-10 02:59:05 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,9 +1,4 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME = "Lucy" 
-OR NAME = "Ella" 
-OR Name = "Pickle" 
-OR Name = "Rogan" 
-OR Name= "Sabrina" 
-OR Name = "Mitty"
+WHERE NAME IN ('Lucy', 'Ella' ,'Pickle','Rogan','Sabrina','Mitty')
