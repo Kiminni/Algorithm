28212d3eeae05125994cd2a406ff3b5c7dfc1f7f
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-03-10 16:26:36 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,12 +1,8 @@
 def solution(n):
-    answer = 0
-    tmp = []    
-    while n > 0 :
-        tmp.append(n % 3)
+    tmp = ''
+    while n:
+        tmp += str(n % 3)
         n = n // 3
-    tmp = tmp[::-1]
-    for i in range(len(tmp)):
-        answer += tmp[i] * (3**i)
+
+    answer = int(tmp, 3)
     return answer
-        
-    
