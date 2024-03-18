bbc69c41edbfc11cf70dd7fd41aaeb220d8df29a
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-03-18 17:19:03 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -4,7 +4,5 @@ def solution(food):
     for i in range(len(food)):
         for j in range(food[i] // 2):
             tmp.append(str(i))
-    tmp_rev = reversed(tmp)
-    tmp.append('0')
-    
-    return ''.join(tmp) + ''.join(tmp_rev)
+
+    return ''.join(tmp) + '0' + ''.join(reversed(tmp))
