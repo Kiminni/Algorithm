b7058cc8e6831bbda36d2486f2939ac8ec704385
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-03-26 02:06:36 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,6 +1,10 @@
 import datetime
 def solution(a, b):
     
-    cal_list = ['MON','TUE','WED','THU','FRI','SAT','SUN']
-    
-    return cal_list[datetime.datetime(2016, a, b).weekday()]
+    month = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    day = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+    date = b
+    for i in range(a-1):
+        date +=month[i]
+
+    return day[date%7]
