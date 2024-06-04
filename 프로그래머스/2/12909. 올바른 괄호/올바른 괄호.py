9040927180b4c17a262b95f66fd73d196cbc31c3
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-06-04 15:39:00 +0900

    [level 2] Title: 올바른 괄호, Time: 9.71 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,7 +1,3 @@
-"""
-1. 문자열을 하나씩 stack에 집어 넣고
-2. 어차피 ( 아니면 ) 이니까 왼쪽이면 left + 1, 오른쪽이면 right + 1
-"""
 def solution(s):
     stack = []
     for i in s:
@@ -9,13 +5,11 @@ def solution(s):
             stack.append(i)
         elif i == ")" and stack == []:
             return False
-        elif i == ")" and "(" in stack:
+        elif i == ")" and stack != []:
             stack.pop()
-                 
-    if stack == []:
-        return True
     
-    return False
-        
+    if stack != []:
+        return False
     
+    return True
     
