b79f8d2cd302e5531ff6126010481ed1d6ea5415
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-06-17 16:44:49 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,27 +1,19 @@
-'''
-1. progresses의 길이가 0일때까지 반복 -> 맨 첫번째 요소가 100을 넘을 때까지
-2. 100을 넘으면 count 추가
-3. 그렇지 않으면 count 0으로 변경, 
-'''
-
 def solution(progresses, speeds):
     time = 0
-    speed = 0
     count = 0
     answer = []
-    
-    while(len(progresses) > 0):
-        if progresses[0] + speeds[0] * time >= 100:
+    while len(progresses) > 0:
+        if progresses[0] + time * speeds[0] >= 100 :
             progresses.pop(0)
             speeds.pop(0)
             count += 1
         else:
-            if count > 0 :
+            if count > 0:
                 answer.append(count)
                 count = 0
+                time = 0
             else:
                 time += 1
-    
-    answer.append(count)
+
+    answer.append(count)            
     return answer
-            
