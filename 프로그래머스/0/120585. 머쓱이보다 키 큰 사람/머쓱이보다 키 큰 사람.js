30ac0a99b7f5f8d22395cc8d7c7044d2f9bb6d73
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-06-29 00:00:29 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,5 @@
 function solution(array, height) {
-    let answer = 0
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > height) { 
-            answer += 1 
-        }
-    }
-    return answer
+    let result = 0
+    array.map((arr) => height < arr && result++);
+    return result;
 }
