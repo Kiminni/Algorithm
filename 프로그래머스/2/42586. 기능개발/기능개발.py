dca713b1488d5b957fd918295c35293892bea253
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-08-16 04:33:38 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,19 +1,20 @@
 def solution(progresses, speeds):
-    time = 0
-    count = 0
     answer = []
-    while progresses:
-        if progresses[0] + time * speeds[0] >= 100:
+    progress = 0
+    speed = 0
+    time = 0
+    tmp = 0
+    while len(progresses) > 0:
+        if progresses[0] + speeds[0] * time >= 100:
+            tmp += 1
             progresses.pop(0)
             speeds.pop(0)
-            count += 1
+            continue
         else:
-            if count > 0 :
-                answer.append(count)
-                count = 0
-                time = 0
-            else:
-                time += 1
-    answer.append(count)
+            if tmp != 0 :
+                answer.append(tmp)
+            tmp = 0
+        time += 1
+    answer.append(tmp)
     return answer
             
