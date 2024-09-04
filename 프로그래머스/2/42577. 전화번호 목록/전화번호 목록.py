2b523c4bb97cc890c8ae7a08ca98ca4888eee916
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-04 16:22:59 +0900

    [level 2] Title: 전화번호 목록, Time: 499.99 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,18 @@
 def solution(phone_book):
-    phone_book.sort()
-    for i in range(len(phone_book) - 1): 
-        if phone_book[i + 1].startswith(phone_book[i]):
-            return False
+    # 딕셔너리에 전화번호를 저장합니다.
+    table = {}
+    
+    # 전화번호를 딕셔너리에 저장합니다.
+    for phone in phone_book:
+        table[phone] = True
+    
+    # 모든 전화번호를 순회합니다.
+    for phone in phone_book:
+        # 접두어를 확인하기 위해 각 전화번호의 부분 문자열을 순회합니다.
+        for i in range(1, len(phone)):
+            # 접두어가 딕셔너리에 있는지 확인합니다.
+            if phone[:i] in table:
+                return False  # 접두어가 발견되면 False를 반환합니다.
+    
+    # 접두어가 없으면 True를 반환합니다.
     return True
