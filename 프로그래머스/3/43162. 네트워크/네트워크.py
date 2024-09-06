a284ebd09c61f4f412c6b760edbe54b12e06c735
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-06 16:00:14 +0900

    [level 3] Title: 네트워크, Time: 2.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,28 +1,26 @@
 def solution(n, computers):
-    # 초기세팅
-    n = len(computers)
-    answer = 0
     graph = {}
     visited = [False] * n
+    answer = 0
+    
     for i in range(n):
-        graph[i] = [] 
-    for i in range(n):
+        tmp = []
         for j in range(n):
             if i != j and computers[i][j] == 1:
-                graph[i].append(j)
-                
-    # 모든 vertex에 대해서 방문한적 없으면 dfs 실행하고 count += 1
+                tmp.append(j)
+            graph[i] = tmp
+    
     for cur_v in range(n):
         if not visited[cur_v]:
-            #bfs()
-            dfs(cur_v, graph, visited)
+            dfs(cur_v, visited, graph)
             answer += 1
-
-    
     return answer
 
-def dfs(cur_v, graph, visited):
+
+def dfs(cur_v, visited, graph):
     visited[cur_v] = True
     for next_v in graph[cur_v]:
         if not visited[next_v]:
-            dfs(next_v, graph, visited)
+            dfs(next_v, visited, graph)
+    
+
