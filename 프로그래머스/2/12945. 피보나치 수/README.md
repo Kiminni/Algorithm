77dfbaf1062ad366e7c1cdc27c627b5062cfc7c8
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-08 16:14:31 +0900

    [level 2] Title: 피보나치 수, Time: 469.95 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
 
 ### 성능 요약
 
-메모리: 456 MB, 시간: 476.24 ms
+메모리: 456 MB, 시간: 469.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 04일 19:29:27
+2024년 09월 08일 16:14:28
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,11 +1,8 @@
 def solution(n):
-    dp = [0] * (n+1)
-    def fibo(n):
-        dp[2] = 1
-        dp[3] = 2
-        
-        for i in range(4, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
-    
-    return fibo(n) % 1234567
+    answer = []
+    for i in range(n + 1):
+        if i == 0 or i == 1:
+            answer.append(i)
+        else:
+            answer.append(answer[i - 1] + answer[i - 2])
+    return answer[-1] % 1234567
