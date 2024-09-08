a40ab5cf53a42f9a9087ed1844b75011e945dc03
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-08 14:55:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.51 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,20 +1,8 @@
 from collections import Counter
-
 def solution(participant, completion):
-    dic = {}
-    for p in participant:
-        if p in dic:
-            dic[p] += 1
-        else:
-            dic[p] = 1
-        
-    for c in completion:
-        dic[c] -= 1
+    answer = Counter(participant) - Counter(completion)
+    return list(answer)[0]
     
-    for p in participant:
-        if dic[p] > 0:
-            return p
-            
 
 
 
