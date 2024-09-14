a5e4614581f37a4a55f83a88d305b2653d3e0f7e
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-14 18:49:26 +0900

    [level 2] Title: 괄호 회전하기, Time: 70.70 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,14 +1,14 @@
 def solution(s):
     answer = 0
     n = len(s)
-    new_s = s[:]
+    new_s = s + s
+    
     # 문자열 길이만큼 반복
-    for _ in range(n):
+    for i in range(n):
         # 유효한지 판별
-        if isValid(new_s):
+        if isValid(new_s[i:i+n]):
             answer += 1
         # 괄호 회전하기    
-        new_s = new_s[1:] + new_s[0]
         
     return answer
 
