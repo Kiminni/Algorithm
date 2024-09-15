c573b98a73462b82500a3573075f389074a30fc2
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-15 14:45:29 +0900

    [level 2] Title: 소수 찾기, Time: 9.75 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,3 +1,4 @@
+from itertools import permutations
 def isPrime(n):
     if n < 2:
         return False
@@ -11,15 +12,24 @@ def solution(numbers):
     nums = set()
     picked = [False] * n
     answer = 0
+    temp = []
+    for i in range(1, n + 1):
+         temp += list(set(permutations(numbers, i)))
+    print(temp)
+    for t in temp:
+        if isPrime(int(''.join(t))):
+            if t[0] =="0":
+                continue
+            answer += 1
     
-    def permute(cur_number):
-        for i in range(n):
-            if not picked[i]:
-                picked[i] = True
-                nums.add(cur_number * 10 + int(numbers[i]))
-                permute(cur_number * 10 + int(numbers[i]))
-                picked[i] = False
-    permute(0)        
+    # def permute(cur_number):
+    #     for i in range(n):
+    #         if not picked[i]:
+    #             picked[i] = True
+    #             nums.add(cur_number * 10 + int(numbers[i]))
+    #             permute(cur_number * 10 + int(numbers[i]))
+    #             picked[i] = False
+    # permute(0)        
     
     for num in nums:
         if isPrime(num):
