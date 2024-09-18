2610619724d3427385a19c8684e2ecc249dd5a6d
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-18 18:06:51 +0900

    [level 2] Title: 소수 찾기, Time: 12.52 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,40 +1,23 @@
-from itertools import permutations
 def isPrime(n):
     if n < 2:
         return False
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0 :
             return False
     return True
-
 def solution(numbers):
-    n = len(numbers)
-    nums = set()
-    picked = [False] * n
     answer = 0
-    temp = []
-    for i in range(1, n + 1):
-         temp += list(set(permutations(numbers, i)))
-    print(temp)
-    for t in temp:
-        if isPrime(int(''.join(t))):
-            if t[0] =="0":
-                continue
-            answer += 1
-    
-    # def permute(cur_number):
-    #     for i in range(n):
-    #         if not picked[i]:
-    #             picked[i] = True
-    #             nums.add(cur_number * 10 + int(numbers[i]))
-    #             permute(cur_number * 10 + int(numbers[i]))
-    #             picked[i] = False
-    # permute(0)        
-    
+    nums = set()
+    pick = [False] * len(numbers)
+    def recur(current):
+        for i in range(len(numbers)):
+            if not pick[i]:
+                pick[i] = True
+                nums.add(current * 10 + int(numbers[i]))
+                recur(current * 10 + int(numbers[i]))
+                pick[i] = False
+    recur(0)
     for num in nums:
         if isPrime(num):
             answer += 1
-            
     return answer
-            
-        
