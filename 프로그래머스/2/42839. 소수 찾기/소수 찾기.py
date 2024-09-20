6d9f3cc7de78a5ad452ca3219439ed2492b8a5cf
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-20 17:41:10 +0900

    [level 2] Title: 소수 찾기, Time: 12.54 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,27 @@
-def isPrime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0 :
-            return False
-    return True
 def solution(numbers):
     answer = 0
     nums = set()
-    pick = [False] * len(numbers)
+    n = len(numbers)
+    pick = [False] * n
     def recur(current):
-        for i in range(len(numbers)):
+        for i in range(n):
             if not pick[i]:
                 pick[i] = True
                 nums.add(current * 10 + int(numbers[i]))
                 recur(current * 10 + int(numbers[i]))
                 pick[i] = False
     recur(0)
+    
     for num in nums:
         if isPrime(num):
             answer += 1
     return answer
+
+def isPrime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0 :
+            return False
+    
+    return True
