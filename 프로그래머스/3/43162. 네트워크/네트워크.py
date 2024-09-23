15ebb0d453835e616cb716c5cd6118a43e9684a5
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-24 02:04:53 +0900

    [level 3] Title: 네트워크, Time: 0.78 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,3 +1,29 @@
 def solution(n, computers):
+    graph = {}
     answer = 0
+    n = len(computers)
+    visited = [False] * n
+    for i in range(n):
+        tmp = []
+        for j in range(n):
+            if i!=j and computers[i][j] == 1:
+                tmp.append(j)
+        graph[i] = tmp
+    for current in graph:
+        if not visited[current]:
+            dfs(current, visited, graph)
+            answer += 1
+            
+            
+    
     return answer
+
+
+def dfs(cur_v, visited, graph):
+    visited[cur_v] = True
+    for next_v in graph[cur_v]:
+        if not visited[next_v]:
+            visited[next_v] = True    
+            dfs(next_v, visited, graph)
+            
+        
