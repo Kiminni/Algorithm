b781f0ac0d6d2517c0e4e4b058a0645625c55318
Kiminni <106671921+Kiminni@users.noreply.github.com>
2024-09-27 18:45:35 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,28 +1,27 @@
 from collections import deque
 def solution(begin, target, words):
-    answer = 0
     if target not in words:
         return 0
+    
     def can_convert(word1, word2):
         count = 0
-        for x, y in zip(word1, word2):
-            if x != y:
+        for w1, w2 in zip(word1, word2):
+            if w1 != w2:
                 count += 1
             if count > 1:
                 return False
         return count == 1
-    
     queue = deque([(begin, 0)])
     visited = set([begin])
     
-    while queue:
-        current, steps = queue.popleft()
-        if current == target:
-            return steps
+    while deque:
+        c, s = queue.popleft()
+        if c == target:
+            return s
         
         for word in words:
-            if word not in visited and can_convert(current, word):
+            if word not in visited and can_convert(c, word):
                 visited.add(word)
-                queue.append((word, steps + 1))
-
+                queue.append([word, s + 1])
     return 0
+    
