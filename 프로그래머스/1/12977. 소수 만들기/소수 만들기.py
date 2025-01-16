4f1c18622522ab31603a2ce0c9db92b66faac591
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-01-16 13:48:32 -0800

    [level 1] Title: 소수 만들기, Time: 16.61 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,14 +1,15 @@
 from itertools import combinations
+
 def solution(nums):
     def isPrime(n):
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
+        for i in range(2, int(n **0.5) + 1):
+            if n % i == 0 :
                 return False
         return True
     answer = 0
     com = combinations(nums, 3)
     for c in com:
         tmp = sum(c)
-        if isPrime(tmp): answer += 1
-
+        if isPrime(tmp):
+            answer += 1
     return answer
