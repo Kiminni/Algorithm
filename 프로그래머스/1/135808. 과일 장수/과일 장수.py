cc4c2fed3d7ad42cd53912c3d85e1f27c42d170d
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-05-26 01:47:09 +0900

    [level 1] Title: 과일 장수, Time: 530.09 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,8 +1,25 @@
+"""
+1. 리스트를 높은 순으로 정렬
+2. m개만큼의 사과만 먹고 분리
+3. 최종적으로는 전체 이익을 리턴
+"""
 def solution(k, m, score):
-    score = sorted(score, reverse = True)
+    max_value = 0
+    score = sorted(score, reverse=True)
+    list_score = []
     tmp = []
-    answer = 0
-    for i in range(len(score) // m):
-        tmp = score[i * m :m * (i+1)]
-        answer += min(tmp) * m
-    return answer
+    for i in range(len(score)):
+        if len(tmp) < m:
+            tmp.append(score[i])
+        if len(tmp) == m:
+            list_score.append(tmp)
+            tmp = []
+    
+    if len(tmp) == m:
+        list_score.append(tmp)
+    
+    for l in list_score:
+        max_value += min(l) * m
+    
+    return max_value
+        
