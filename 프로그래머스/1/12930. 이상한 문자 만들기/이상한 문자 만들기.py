9d4c4cd1bfa727b3e36af8a2af4df1c8c28b11f1
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-05-27 14:02:14 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,12 +1,26 @@
+# s는 한 개 이상의 단어로 구성 각 단어에는 공백 문자
+# 짝수번째는 대문자, 홀수번째는 소문자
+
+# 짝 홀수 인덱스가 아니라, 공백 기준으로 짝 홀수를 처리
+# 0번째 인덱스는 짝수 알파벳
+
+# "try hello world" -> "TrY HeLlO WoRld" 
+# T r Y // H e L l o
+# 문자열을 쪼개서 리스트에 넣고
+# 반복문을 진행할 때 새로이 문자열을 넣고
+# 공백이 있다면 문자 마지막에 넣는 식으로 처리
 def solution(s):
-    s = s.split(" ")
-    answer = []
-    for str in s:
-        tmp = []
-        for i in range(len(str)):
-            if i % 2 == 0 :
-                tmp.append(str[i].upper())
+    string_list = s.split(" ") # ["try", "hello", "world"]
+    temp = []
+    for string in string_list: # ["try"]
+        for i in range(len(string)):
+            if i % 2 == 0:
+                temp.append(string[i].upper())
             else:
-                tmp.append(str[i].lower())     
-        answer.append(''.join(tmp))
-    return ' '.join(answer)
+                temp.append(string[i].lower())
+        temp.append(" ")
+    temp.pop()
+    return ''.join(temp)
+    
+    
+    
