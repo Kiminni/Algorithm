40c93669b159004c757662ad131faa864c6d297b
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-05-29 14:38:58 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,10 +1,13 @@
+# 문자열의 길이가 4이거나, 6인지 확인한다.
+# 문자열이 숫자로만 구성되어있는지를 확인한다.
+# 이 때, 알파벳 같은 경우는 a, A 총 2가지의 경우가 있을 수 있으니 소문자로 바꾼다.
 def solution(s):
-
-    for i in s.lower():
-        if 'a' <= i <= 'z':
-            return False
-        
     if len(s) != 4 and len(s) != 6:
         return False
     
+    for string in s:
+        if string.lower() in "abcdefghijklmnopqrstuvwxyz":
+            return False
+        
     return True
+    
