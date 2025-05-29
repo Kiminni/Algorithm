bc5ae2ad2a3a45e8b254f5089dcfd23c638d9dd0
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-05-29 18:02:03 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,15 +1,23 @@
+"""
+정수 배열 / 절댓값 배열
+이걸 실제로 접근해서 해결하는 문제
+[4, 7, 12]
+[t, f, t]
+-> +4 -7 +12
+
+len(signs) == len(absolutes)
+
+반복문을 실행을 해야합니다. 둘의 길이 중 하나를 갖고 반복문을 돌릴겁니다.
+이 때, 이 과정에서 중요한 것은 true인지, false인지를 확인 후 값을 더한다.
+"""
 def solution(absolutes, signs):
-    length = len(signs)
     answer = 0
-    i = 0
-    while i < length :
-        if signs[i] == False:
-            answer -= absolutes[i]
-            
-        else:
+    
+    for i in range(len(absolutes)):
+        if signs[i]:
             answer += absolutes[i]
-        i += 1
-        
-        
+        else:
+            answer -= absolutes[i]
+    
     
     return answer
