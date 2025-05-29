ff3c718725b96e364da794b54562f3a2e1038f88
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-05-30 01:24:07 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 1.40 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,7 +1,23 @@
+"""
+원래 가격 price원
+N번째 이용하면 price 가격의 N배를 받습니다.
+1번째 100 / 2번째 200 / 3번째 300
+count번 탈 때, 얼마가 모자라는지를 반환합니다.
+단, 금액이 부족하지 않다면 0을 반환합니다.
+
+money = 내가 갖고 있는 돈
+1번 money - price *  
+2번 money - price * 1 - price * 2
+3번 money - price * 1 - price * 2 - price * 3
+
+n 번 money - price * 1 - price *2 ... price * n
+"""
 def solution(price, money, count):
-    summation = 0
-    for i in range(1, count + 1):
-        summation += price * i
-    if money - summation > 0 :
+    temp = 0
+    for i in range(1, count + 1): # 3 6 9 12
+        money = money - price * i # 20 - 3 - 6 -9 - 12
+        print(money)
+    if money > 0:
         return 0
-    return summation - money
+    return abs(money)
+    
