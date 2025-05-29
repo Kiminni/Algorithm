327059e3319e8b4da0afd26452a70ce5dda58c2c
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-05-29 17:46:45 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 29.93 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,16 +1,28 @@
+# left, right 주어진다.
+# left -> right 까지의 모든 수 중에서, 약수의 개수가 짝수면 더한다. 그리고, 홀수면 뺀다.
+"""
+1. 13 = 1, 13
+2. 14 = 1, 2, 7, 14
+3. 15 = 1, 3, 5, 15
+4. 16 = 1, 2, 4, 8, 16
+5. 17 = 1, 17
+
+# left ~ right 로 반복문을 실행합니다.
+# 이제 약수의 개수를 확인합니다.
+# 
+"""
 def solution(left, right):
     answer = 0
-    for idx in range(left, right + 1):
-        
-        tmp = 0
-        for i in range(1,idx + 1):
-            if idx % i == 0 :
-                tmp += 1
-                
-        if tmp % 2 == 0:
-            answer += idx
+    for number in range(left, right + 1):
+        temp = 0
+        for div in range(1, number+ 1):
+            if number % div == 0:
+                temp += 1
             
+        if temp % 2 == 0:
+            answer += number
         else:
-            answer -= idx
-            
+            answer -= number
+    
+
     return answer
