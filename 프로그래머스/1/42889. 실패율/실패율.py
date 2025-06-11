19e0a8dc6db77d5838639d1b8649159a3f43dab4
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-06-11 23:01:44 +0900

    [level 1] Title: 실패율, Time: 3253.05 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,17 +1,30 @@
-'''
-    dict를 이용하여 접근해보자.
-'''
+"""
+실패율 = 클리어 X 유저 수 / 스테이지 도달한 플레이어 수
+전체 스테이지 개수 N, 멈춘 번호 stages
+실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담긴 배열 return
+
+스테이지의 개수 1 ~ 500
+길이 1 ~ 200000 => 반복문 한 번
+스테이지 => 1 ~ N + 1
+실패율 같다 = 작은 번호 스테이지
+도달한 유저가 없다 => 실패율 0
+"""
+
 def solution(N, stages):
-    answer = []
     ratio = {}
     player = len(stages)
     
-    for i in range(1, N+1):
+    for i in range(1, N + 1):
         if player == 0:
             ratio[i] = 0
+    
         else:
             ratio[i] = stages.count(i) / player
-            player -= stages.count(i)
+            player = player - stages.count(i)
+            
+    sorted_ratio = sorted(ratio, key=lambda x: ratio[x], reverse=True)
     
-    answer = sorted(ratio, key = lambda x : ratio[x], reverse= True)
-    return answer
+    return sorted_ratio
+
+    
+    
