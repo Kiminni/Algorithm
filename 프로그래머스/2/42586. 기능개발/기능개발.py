9222604a2ae92edacfadba74dae8b4f953e67131
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-06-17 16:44:15 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,9 +1,10 @@
 def solution(progresses, speeds):
     answer = []
     time = 0
-    speed = 0
     progress = 0
+    speed = 0
     tmp = 0
+    
     while len(progresses) > 0:
         if progresses[0] + time * speeds[0] >= 100:
             tmp += 1
@@ -12,10 +13,11 @@ def solution(progresses, speeds):
             continue
         else:
             time += 1
-        if tmp :
+        
+        if tmp:
             answer.append(tmp)
             tmp = 0
-    
+        
     answer.append(tmp)
     return answer
         
