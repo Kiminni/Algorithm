118bbd56725e70c7b469b6a05255c43bac4f5eb5
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-06-17 21:17:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 43.62 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,16 +1,15 @@
 def solution(participant, completion):
     dic = {}
     for p in participant:
-        if p in dic:
-            dic[p] += 1
-        else:
+        if p not in dic:
             dic[p] = 1
+        else:
+            dic[p] += 1
     
     for c in completion:
         if c in dic:
             dic[c] -= 1
     
     for d in dic:
-        if dic[d] != 0 :
+        if dic[d] == 1:
             return d
-    
