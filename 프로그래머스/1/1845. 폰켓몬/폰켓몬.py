9b47641f78006b202f74cbe06640246f177c7c30
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-06-19 22:00:29 +0900

    [level 1] Title: 폰켓몬, Time: 0.73 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,8 @@
 def solution(nums):
     answer = []
-
-    for num in nums:
-        if len(answer) >= len(nums) / 2:
-            break
-        if num not in answer:
-            answer.append(num)
+    return min(len(set(nums)), len(nums) // 2)
     
-    return len(answer)
+    
+    
+    
+        
