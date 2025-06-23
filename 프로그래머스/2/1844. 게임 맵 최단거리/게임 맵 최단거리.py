52fa08472ccb017103a82421f17e605ea9fae05c
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-06-23 22:57:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.79 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -10,18 +10,18 @@ def solution(maps):
     queue.append((0, 0))
     
     while queue:
-        x, y= queue.popleft()
+        x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            
             if 0 <= nx < N and 0 <= ny < M and maps[nx][ny] == 1:
                 maps[nx][ny] = maps[x][y] + 1
                 queue.append((nx, ny))
-    
+        
     answer = maps[N-1][M-1]
     
     if answer == 1:
-        answer = -1
+        return -1
     return answer
-            
-    return answer
+    
