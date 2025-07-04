97b3819910f3860eeb4b9a920ce99ed49fccd9d1
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-07-04 21:11:47 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,26 @@
 from collections import deque
+N,M = map(int, input().split())
+maps = []
+for i in range(N):
+    tmp = input().strip()  # 문자열 그대로
+    l = list(map(int, tmp))  # 문자열의 각 문자를 숫자로 변환
+    maps.append(l)
+def bfs(x, y, graph):
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
 
-N, M = map(int, input().split())
-
-l = []
-for _ in range(N):
-    line = input().strip()
-    row = list(map(int, line))
-    l.append(row)  
-
-def BFS(x, y):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
     queue = deque()
     queue.append((x, y))
     while queue:
         x, y = queue.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < N and 0 <= ny < M and l[nx][ny] == 1:
-                l[nx][ny] = l[x][y] + 1
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
                 queue.append((nx, ny))
+    answer = graph[N-1][M-1]
+    return answer
 
-    return l[N-1][M-1]
-
-print(BFS(0, 0))
+print(bfs(0, 0, maps))
