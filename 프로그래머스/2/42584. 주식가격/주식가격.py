7c510cd7c9e4b4fbd2f18ab09e803c46504752de
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-07-04 19:27:10 +0900

    [level 2] Title: 주식가격, Time: 83.95 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -2,14 +2,12 @@ from collections import deque
 def solution(prices):
     answer = []
     prices = deque(prices)
-    while prices:
-        p = prices.popleft()
-        tmp = 0
-        for price in prices:
-            tmp += 1    
-            if p > price:
+    while prices: 
+        count = 0
+        price = prices.popleft() # 처음에 뺀 값 1 [2, 3, 2, 3]
+        for p in prices:
+            count += 1
+            if price > p :
                 break
-        
-        answer.append(tmp)
-    
+        answer.append(count)
     return answer
