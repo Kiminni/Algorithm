3deed96b5ead7d7548aaf9c26657f530789651d2
Kiminni <106671921+Kiminni@users.noreply.github.com>
2025-07-08 21:48:39 +0900

    [level 2] Title: 피보나치 수, Time: 534.17 ms, Memory: 456 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,6 +1,5 @@
 def solution(n):
-    fib = [0, 1, 1, 2]
-    for i in range(4, n + 1):
-        fib.append(fib[i - 2] + fib[i - 1])
-        
+    fib = [0, 1]
+    for i in range(2, n + 1):
+        fib.append(fib[i - 1] + fib[i - 2])
     return fib[n] % 1234567
